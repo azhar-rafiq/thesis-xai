7bb5502972fbc3b4a0feaf03da9a872850715448
Azhar Rafiq <axr1222@student.bham.ac.uk>
2026-05-20 11:47:52 +0100

    Basic classification done for RSNA pipeline with sbatch for training. The rest in notebook. Using logger to monitor training results.

---
+++ 1_train_mult_rsna.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=karwatha-karwath-hds-pg-research
+#SBATCH --qos=bbgpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=12:00:00
+#SBATCH --mem=128G
+#SBATCH --cpus-per-task=8
+#SBATCH --output=train_%j.log
+#SBATCH --job-name=rsna-train
+
+echo "Job started: $(date)"
+echo "Node: $(hostname)"
+echo "Job ID: $SLURM_JOB_ID"
+
+module purge
+module load bluebear
+module load bear-apps/2024a/live
+module load CUDA/12.6.0
+module load cuDNN/9.8.0.87-CUDA-12.6.0
+
+source ~/thesis-xai/thesis-venv/bin/activate
+
+python ~/thesis-xai/1_train_mult_rsna.py
+
+echo "Job finished: $(date)"
